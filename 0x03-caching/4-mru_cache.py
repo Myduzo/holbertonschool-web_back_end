acd7d0d7de410c1f78a3777cb65cb608aa7f5287
Myduzo <sahli.youssef@outlook.com>
2021-03-22 11:13:46 +0100

    Cache

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -14,23 +14,20 @@ class MRUCache(BaseCaching):
     def put(self, key, item):
         """PUT method with Most Recently Used queue"""
         if key and item:
-            # print(self.Queue)
             if key in self.cache_data:
                 self.cache_data[key] = item
-                self.queue.remove(key)
-                self.queue.insert(0, key)
             else:
-                if (len(self.cache_data) == self.MAX_ITEMS):
-                    print("DISCARD: {}".format(self.queue[0]))
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    print('DISCARD: ' + self.queue[0])
                     del self.cache_data[self.queue[0]]
                     del self.queue[0]
-                self.queue.insert(0, key)
                 self.cache_data[key] = item
+                self.queue.insert(0, key)
 
     def get(self, key):
         """GET method"""
         if key in self.cache_data:
             self.queue.remove(key)
-            self.queue[:0] = key
+            self.queue.insert(0, key)
             return self.cache_data[key]
         return None
