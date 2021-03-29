656d257c73db3fc459b08692d9f5e6e5ab800683
Myduzo <sahli.youssef@outlook.com>
2021-03-29 19:37:08 +0100

    PII

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -25,7 +25,7 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """Log formatter"""
         return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
+                            super().format(record),
                             self.SEPARATOR)
 
 
