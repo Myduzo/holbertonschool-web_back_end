7a7c82b507ae571aee2e84b06c068692de7447ce
Myduzo <sahli.youssef@outlook.com>
2021-03-29 19:25:01 +0100

    PII

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -33,8 +33,8 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str):
     """Regex-ing"""
     for field in fields:
-        message = re.sub(field + "=.+?" + separator,
-                         field + "=" + redaction + separator, message)
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
 
 
