535a97d75152b1128419f8cf45f6d33ccce99dbb
Myduzo <sahli.youssef@outlook.com>
2021-03-29 19:06:07 +0100

    PII

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Personal data PII and non PII
+"""
+from typing import List
+import re
+import logging
+import os
+import mysql.connector
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """Constructor method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Log formatter"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                    separator: str):
+    """Regex-ing"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                            field + "=" + redaction + separator, message)
+    return message
+
+def get_logger() -> logging.Logger:
+    """Create logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+
+    logger.propagate = False
+    formatter = RedactingFormatter(PII_FIELDS)
+
+    file_handler = logging.StreamHandler()
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connect to secure database"""
+    cnn = mysql.connector.connection.MySQLConnection(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME'))
+    return cnn
+
+def main():
+    
