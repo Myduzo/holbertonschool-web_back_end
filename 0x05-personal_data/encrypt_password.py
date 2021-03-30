ce303132d8a49d8dcb0e401325327e0c39cb02d2
Myduzo <sahli.youssef@outlook.com>
2021-03-30 16:21:29 +0100

    hashing

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,7 +9,13 @@ def hash_password(password: str) -> bytes:
     one string argument name password and returns a salted,
     hashed password, which is a byte string."""
     passwd = password.encode()
-
     salt = bcrypt.gensalt()
+
     hashed = bcrypt.hashpw(passwd, salt)
     return hashed
+
+def is_valid(hashed_password: bytes, password:str) -> bool:
+    pwd = password.encode()
+    if bcrypt.checkpw(pwd, hashed_password):
+        return True
+    return False
