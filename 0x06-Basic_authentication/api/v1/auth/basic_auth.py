dbac903df8b68aa12b3340f46f4958931cda204b
Myduzo <sahli.youssef@outlook.com>
2021-04-01 22:00:07 +0100

    authentication

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,19 +10,25 @@ from models.user import User
 class BasicAuth(Auth):
     """BasicAuth class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """returns the Base64 part of the Authorization header
         for a Basic Authentication
         """
         auth = authorization_header
 
-        if auth is None or not isinstance(auth, str)  or not auth.startswith('Basic '):
+        if auth is None or not isinstance(auth, str):
+            return None
+
+        if not auth.startswith('Basic '):
             return None
 
         return auth[6:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        """returns the decoded value of a Base64 string base64_authorization_header
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header
         """
         base_auth = base64_authorization_header
 
@@ -34,7 +40,8 @@ class BasicAuth(Auth):
         except Exception as e:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """returns the user email and password from the Base64 decoded value.
         """
         decode_base_auth = decoded_base64_authorization_header
@@ -49,7 +56,8 @@ class BasicAuth(Auth):
 
         return data[0], data[1]
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """returns the User instance based on his email and password.
         """
         if user_email is None or not isinstance(user_email, str):
@@ -58,7 +66,6 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        
         try:
             db_user = User.search({'email': user_email})
         except Exception as e:
