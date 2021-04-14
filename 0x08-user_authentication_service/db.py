782c672f58067dce5db6d4e74b848f7a652be447
Myduzo <sahli.youssef@outlook.com>
2021-04-14 12:06:56 +0100

    ORM

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -59,7 +59,7 @@ class DB:
         user = self.find_user_by(id=user_id)
 
         for k, v in kwargs.items():
-            if k is None:
+            if not hasattr(user, k):
                 raise ValueError
             else:
                 setattr(user, k, v)
