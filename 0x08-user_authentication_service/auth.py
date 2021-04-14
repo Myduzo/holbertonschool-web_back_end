e06fde58d3179a14bcb4d986a31057bb3190a6d3
Myduzo <sahli.youssef@outlook.com>
2021-04-14 13:39:56 +0100

    authentication

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -63,6 +63,31 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Instance method that takes a single session_id
+        string argument and returns the corresponding User or None.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ The method takes a single user_id
+        integer argument and returns None.
+        """    
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user.id, session_id=None)
+            if session_id is None:
+                return None
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """ Private method that returns
