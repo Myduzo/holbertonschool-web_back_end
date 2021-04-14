1f9d8f9d47783d5c93aa235a695c79e00c066635
Myduzo <sahli.youssef@outlook.com>
2021-04-14 15:18:40 +0100

    requests

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -44,12 +44,14 @@ def login() -> str:
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout() -> None:
     session_id = request.cookies.get('session_id')
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
-    except ValueError:
+    else:
         abort(403)
+
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
