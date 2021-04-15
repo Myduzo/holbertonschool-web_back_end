036b9ef62807d2016b1bff5ed198525ed46e8f88
Myduzo <sahli.youssef@outlook.com>
2021-04-15 14:12:39 +0100

    authentication

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -90,15 +90,15 @@ def get_reset_password_token() -> str:
 def update_password() -> str:
     """ Updates the user password
     """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-
     try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
-    except:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+    except ValueError:
         abort(403)
+
+    AUTH.update_password(reset_token, new_password)
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
