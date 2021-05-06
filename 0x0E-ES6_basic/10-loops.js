2c498badd354a51b21736e9daf51caedc99356f5
Myduzo <sahli.youssef@outlook.com>
2021-05-06 18:37:45 +0100

    ES6

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (const value of array) {
+    /* eslint-disable */
     array[idx] = appendString + value;
+    /* eslint-enable */
     idx += 1;
   }
 
