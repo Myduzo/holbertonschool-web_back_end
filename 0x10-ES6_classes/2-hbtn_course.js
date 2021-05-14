37e914a7b7ddac883a7d7937ef7f39232bd1759d
Myduzo <sahli.youssef@outlook.com>
2021-05-14 22:51:32 +0100

    tasks 0,1,2,3

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw TypeError('Students must be an array of strings');
+    if (students.every((idx) => typeof idx !== 'string')) throw TypeError('Students must be an array of strings');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setter
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students) || students.every((idx) => typeof idx !== 'string')) {
+        throw TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
+}
